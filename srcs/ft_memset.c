7aa0bdea7cb116f98d07381727539a2912851bd5
albgarci <albgarci@student.42madrid.com>
2021-09-13 19:36:51 +0200

    .

---
+++ srcs/ft_memset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgarci <albgarci@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/13 19:09:27 by albgarci          #+#    #+#             */
+/*   Updated: 2021/09/13 19:30:26 by albgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t	i;
+	char	*s;
+
+	i = 0;
+	c = (unsigned char) c;
+	s = (char *) b;
+	while(i < len)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (b);
+}
