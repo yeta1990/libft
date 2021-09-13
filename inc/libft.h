38bb25a424a6df5d43939949981b02b77149eed0
albgarci <albgarci@student.42madrid.com>
2021-09-13 19:08:01 +0200

    strlen

--- inc/libft.h
+++ inc/libft.h
@@ -1,10 +1,13 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+#include <stdio.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
 
 #endif
