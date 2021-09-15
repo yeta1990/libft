cb32201329b148f90c19a02a85c8418fbebe2130
albgarci <albgarci@student.42madrid.com>
2021-09-15 16:50:53 +0200

    strchr, strlcat, strrchr, tolower, toupper

---
+++ ft_strlcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgarci <albgarci@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/13 11:19:30 by albgarci          #+#    #+#             */
+/*   Updated: 2021/09/15 16:02:27 by albgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, char *src, size_t size)
+{
+	size_t	destlen;
+	size_t	srclen;
+	size_t	i;
+
+	i = 0;
+	destlen = 0;
+	srclen = 0;
+	while (dest[destlen] && destlen < size)
+		destlen++;
+	while (src[srclen])
+		srclen++;
+	if (destlen == size)
+		return (size + srclen);
+	if (size > destlen)
+	{
+		while (src && src[i] && i < size - destlen - 1)
+		{
+			dest[destlen + i] = src[i];
+			i++;
+		}
+		dest[destlen + i] = '\0';
+	}
+	return (destlen + srclen);
+}
--- libft.h
+++ libft.h
@@ -13,5 +13,11 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dest, char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 
 #endif
