46ae880fa7951c49ef2045f7bf59edca31667e6c
albgarci <albgarci@student.42madrid.com>
2021-09-16 17:38:15 +0200

    memchr, memcmp, strnstr ok

---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgarci </var/spool/mail/albgarci>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 03:39:49 by albgarci          #+#    #+#             */
+/*   Updated: 2021/09/16 03:49:08 by albgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	i = 0;
+	c = (unsigned char) c;
+	str = (unsigned char *) s;
+	while (i < n)
+	{
+		if (c == str[i])
+			return (str + i);
+		i++;
+	}
+	return (0);
+}
---
+++ ft_strnstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgarci <albgarci@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 15:29:26 by albgarci          #+#    #+#             */
+/*   Updated: 2021/09/16 17:36:12 by albgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
+	{
+		if (haystack[i] == needle[0])
+		{
+			while (needle[++j])
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					j = 0;
+					break ;
+				}
+			}
+			if (j && (i + j) <= len)
+				return ((char *)(haystack + i));
+		}
+		i++;
+	}
+	return (0);
+}
