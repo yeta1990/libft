8cce535c6fab079fb46074d072430059e39b0db6
albgarci <albgarci@student.42madrid.com>
2021-09-16 19:27:11 +0200

    calloc

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgarci <albgarci@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 19:02:08 by albgarci          #+#    #+#             */
+/*   Updated: 2021/09/16 19:24:45 by albgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*str;
+
+	str = malloc(size * count);
+	if (!str)
+		return (0);
+	ft_bzero(str, count * size);
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -2,6 +2,7 @@
 # define LIBFT_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -24,5 +25,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
