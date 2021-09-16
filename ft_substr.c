5478d19c9c18232c4acffc44756d69accbd9975e
albgarci <albgarci@student.42madrid.com>
2021-09-16 20:09:06 +0200

    substr

---
+++ ft_substr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgarci <albgarci@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 19:46:51 by albgarci          #+#    #+#             */
+/*   Updated: 2021/09/16 20:08:16 by albgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	size;
+	char	*s2;
+
+	if (!s)
+		return (0);
+	if (start >= ft_strlen(s))
+	{
+		s2 = malloc(1);
+		return (s2);
+	}
+	if (len > ft_strlen(s))
+		size = ft_strlen(s) - start + 1;
+	else
+		size = len + 1;
+	s2 = malloc(sizeof(char) * size);
+	if (!s2)
+		return (0);
+	s2 = ft_memcpy(s2, (s + start), len);
+	s2[len] = '\0';
+	return (s2);
+}
