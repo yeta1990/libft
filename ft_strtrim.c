be5b7ab4bd9f12be4cdbdc31c8e41b6012c672ab
albgarci <albgarci@student.42madrid.com>
2021-09-17 14:13:28 +0200

    ft_strtrim with 1 mem error

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,14 +6,11 @@
 /*   By: albgarci <albgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 10:18:26 by albgarci          #+#    #+#             */
-/*   Updated: 2021/09/17 12:01:29 by albgarci         ###   ########.fr       */
+/*   Updated: 2021/09/17 14:12:31 by albgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-//static int ft_first_oc(char *s1, char *set);
-//static int ft_last_oc(char *s1, char *set);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -24,11 +21,16 @@ char	*ft_strtrim(char const *s1, char const *set)
 	i = 0;
 	j = ft_strlen(s1);
 	if (!s1 || !set)
-		return ((char *)s1);
+		return (NULL);
 	while (ft_strchr(set, s1[i]))
 		i++;
-	while (ft_strrchr(set, s1[j]))
+	while (j > i && ft_strrchr(set, s1[j]))
 		j--;
+/*	if (i > j)
+	{
+		s = ft_substr(s1, 0, 1);
+		return (s);
+	}*/
 	s = ft_substr(s1, i, j - i + 1);
 	if (!s)
 		return (0);
