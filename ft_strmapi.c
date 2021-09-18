62cdc271cd061b1ffd0a3473d0b5bb6fad7ed2a9
albgarci <albgarci@student.42madrid.com>
2021-09-18 20:45:17 +0200

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgarci <albgarci@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/18 20:30:28 by albgarci          #+#    #+#             */
+/*   Updated: 2021/09/18 20:44:56 by albgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*s2;
+
+	i = 0;
+	s2 = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!s2)
+		return (0);
+	while (s[i])
+	{
+		s2[i] = f(i, s[i]);
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
