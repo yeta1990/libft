7a50e9e797834ad91beec9197624db328f455269
Alberto <yeta1990@gmail.com>
2021-09-19 04:03:11 +0200

    trying to protect mem

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: albgarci <albgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 10:18:26 by albgarci          #+#    #+#             */
-/*   Updated: 2021/09/18 21:38:35 by albgarci         ###   ########.fr       */
+/*   Updated: 2021/09/19 04:01:27 by albgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	i = 0;
 	j = ft_strlen(s1);
-	if (!s1 || !set)
+	if (!s1)
 		return (NULL);
+	if (!set && s1)
+		return (ft_strdup(s1));
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
 	while (j > i && ft_strrchr(set, s1[j]))
