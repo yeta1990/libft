28b6c8e1a17154efff728b327c7b40def9af3da6
Alberto <yeta1990@gmail.com>
2021-09-19 15:06:33 +0200

    lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgarci </var/spool/mail/albgarci>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/19 13:59:03 by albgarci          #+#    #+#             */
+/*   Updated: 2021/09/19 15:06:17 by albgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*aux;
+	t_list	*aux_next;
+
+	aux = *lst;
+	while (aux)
+	{
+		aux_next = aux->next;
+		del(aux->content);
+		free(aux);
+		aux = aux_next;
+	}
+	*lst = NULL;
+}
